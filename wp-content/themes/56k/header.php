df7f2c476fddd0e30b802807de6eea64a89e82ba
Brad Sawicki <git@bradsawicki.com>
2018-12-14 09:11:08 +0100

    GTM snippets header

--- wp-content/themes/56k/header.php
+++ wp-content/themes/56k/header.php
@@ -4,9 +4,23 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta content="width=device-width, initial-scale=1.0, user-scalable=1, minimum-scale=1.0" name="viewport">
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TXNG7TT');</script>
+	<!-- End Google Tag Manager -->
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXNG7TT"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 	<?php get_template_part( 'partials/svg' ); ?>
 
 	<header class="header">
